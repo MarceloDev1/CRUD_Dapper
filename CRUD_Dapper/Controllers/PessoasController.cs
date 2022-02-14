a31d2a67a0dc575eedc282c30bbccf5e7956418f
Marcelo Rocha Gomes <marcelokm15@gmail.com>
2022-02-14 20:57:39 -0300

    first commit

---
+++ CRUD_Dapper/Controllers/PessoasController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CRUD_Dapper.Models;
+using System.Threading.Tasks;
+using System.Data;
+using Npgsql;
+using Dapper;
+
+namespace CRUD_Dapper.Controllers
+{
+    public class PessoasController : Controller
+    {
+        private readonly string ConnectionString = "User ID=postgres;Password=admin;Host=localhost;Port=5432;Database=PessoasDB;";
+        public IActionResult Index()
+        {
+            IDbConnection con;
+            try
+            {
+                string selecaoQuery = "SELECT * FROM pessoas";
+                con = new NpgsqlConnection(ConnectionString);
+                con.Open();
+                IEnumerable<Pessoas> listaPessoas = con.Query<Pessoas>(selecaoQuery).ToList();
+                return View(listaPessoas);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Pessoas pessoas)
+        {
+            if (ModelState.IsValid)
+            {
+                IDbConnection con;
+
+                try
+                {
+                    string insersaoQuery = "INSERT INTO pessoas(nome, idade, peso) VALUES (@Nome, @Idade, @Peso)";
+                    con = new NpgsqlConnection(ConnectionString);
+                    con.Open();
+                    con.Execute(insersaoQuery, pessoas);
+                    con.Close();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return View(pessoas);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int pessoaid)
+        {
+            IDbConnection con;
+            try
+            {
+                string selecaoQuery = "SELECT * FROM pessoas WHERE pessoaId = @pessoaId";
+                con = new NpgsqlConnection(ConnectionString);
+                con.Open();
+                Pessoas pessoas = con.Query<Pessoas>(selecaoQuery, new { pessoasid = pessoaid }).FirstOrDefault();
+                con.Close();
+
+                return View(pessoas);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int pessoaid, Pessoas pessoas)
+        {
+            if (pessoaid != pessoas.PessoaId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                IDbConnection con;
+
+                try
+                {
+                    con = new NpgsqlConnection(ConnectionString);
+                    string atualizarQuery = "UPDATE pessoas SET Nome=@Nome, Idade=@Idade, Peso=@Peso WHERE PessoaId=@PessoaId";
+                    con.Open();
+                    con.Execute(atualizarQuery, pessoas);
+                    con.Close();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return View(pessoas);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int pessoaid)
+        {
+            IDbConnection con;
+
+            try
+            {
+                string excluirQuery = "DELETE FROM pessoas WHERE PessoaId=@PessoaId";
+                con = new NpgsqlConnection(ConnectionString);
+                con.Open();
+                con.Execute(excluirQuery, new { PessoaId = pessoaid });
+                con.Close();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ CRUD_Dapper/Models/Pessoas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Dapper.Models
+{
+    public class Pessoas
+    {
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public float Peso { get; set; }
+    }
+}
